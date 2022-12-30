3d1c6378c4bdb3d3b622ab67a911fe04438f13ee
Felix <zykinf@gmail.com>
2022-12-30 13:44:32 +0500

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+<div id="container">
+    <h1>HEADER</h1>
+</div>
+</template>
+<style scoped>
+#container{
+    width: 100%;
+    border: 1px solid black;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="container">
+    <Header />
+</div>
+</template>
+<style scoped>
+.container{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+</style>
